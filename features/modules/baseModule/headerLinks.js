55bd7084d54b64959ade1054178923b1d7c2c85c
riaxscorpion <riaxscorpion@gmail.com>
2020-12-31 17:22:01 +0530

    cm

--- features/modules/baseModule/headerLinks.js
+++ features/modules/baseModule/headerLinks.js
@@ -8,7 +8,6 @@ module.exports = {
         await browser.sleep(3000);
         await element(by.linkText(obj.baseModule.headerLinks.APIlink)).click();
         await browser.sleep(3000);
-        await console.log("Changed from gitHub");
     },
 
     clickApplicationsHeader: async function(){
